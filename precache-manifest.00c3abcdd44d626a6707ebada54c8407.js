32603c87856bd2a867feaad7cd754b395d073f94
coureur12 <80371972+coureur12@users.noreply.github.com>
2022-01-10 08:33:51 +0100

    Updates

---
+++ precache-manifest.00c3abcdd44d626a6707ebada54c8407.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf70c378361459fb2995d8f0b2dbc20",
+    "url": "/crowdfunding-page/index.html"
+  },
+  {
+    "revision": "c8345781fac57cd63113",
+    "url": "/crowdfunding-page/static/css/main.e89a5ae0.chunk.css"
+  },
+  {
+    "revision": "e65e97eb10e25b5be429",
+    "url": "/crowdfunding-page/static/js/2.cfcd126d.chunk.js"
+  },
+  {
+    "revision": "69479d3d7550ac7572aa",
+    "url": "/crowdfunding-page/static/js/3.0711f24a.chunk.js"
+  },
+  {
+    "revision": "c8345781fac57cd63113",
+    "url": "/crowdfunding-page/static/js/main.66507ce7.chunk.js"
+  },
+  {
+    "revision": "4cc5a0c62f368d45b485",
+    "url": "/crowdfunding-page/static/js/runtime~main.be587ced.js"
+  },
+  {
+    "revision": "7b0ab1a360478af7171ed5783343789d",
+    "url": "/crowdfunding-page/static/media/icon-close-menu.7b0ab1a3.svg"
+  },
+  {
+    "revision": "7f6e2a1747ed58c8d628a9bfd1f28f86",
+    "url": "/crowdfunding-page/static/media/image-hero-desktop.7f6e2a17.jpg"
+  },
+  {
+    "revision": "0ca19c3b43e02d3ffc2518241a0cf58e",
+    "url": "/crowdfunding-page/static/media/image-hero-mobile.0ca19c3b.jpg"
+  }
+]);
